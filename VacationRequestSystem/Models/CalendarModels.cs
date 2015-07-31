c1bf0f9f9b2f5dcbc1085909b3923f9bc2c37597
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-08-01 00:01:21 +0300

    added Calendar Model

---
+++ VacationRequestSystem/Models/CalendarModels.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace VacationRequestSystem.Models
+{
+    public class CalendarModel
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int[] Holidays { get; set; }
+        public int[] WorkingWeekends { get; set; }
+        public int[] PendingRequests { get; set; }
+        public int[] ApprovedRequests { get; set; }
+    }
+}

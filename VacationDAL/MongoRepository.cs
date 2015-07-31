f7ede55588843b0b07ac1936e90d87252f3b6419
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-07-31 17:01:20 +0300

    added DAL project

---
+++ VacationDAL/MongoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacationDAL
+{
+    public class MongoRepository
+    {
+        //private IMongoDatabase _provider;
+    }
+}

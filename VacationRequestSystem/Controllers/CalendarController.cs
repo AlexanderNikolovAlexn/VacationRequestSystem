c93287f939d394c7a0cf752d00f5cc97a0af8a61
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-08-01 18:34:25 +0300

    added partial view and changed html display logic

---
+++ VacationRequestSystem/Controllers/CalendarController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VacationRequestSystem.Models;
+
+namespace VacationRequestSystem.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult DisplayCalendar(int userId)
+        {
+            CalendarModel calendar = new CalendarModel();
+
+            return PartialView("_CalendarPartial", calendar);
+        }
+    }
+}
--- VacationRequestSystem/Models/CalendarModels.cs
+++ VacationRequestSystem/Models/CalendarModels.cs
@@ -2,16 +2,25 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
+using System;
 
 namespace VacationRequestSystem.Models
 {
     public class CalendarModel
     {
-        public int Year { get; set; }
-        public int Month { get; set; }
-        public int[] Holidays { get; set; }
-        public int[] WorkingWeekends { get; set; }
-        public int[] PendingRequests { get; set; }
-        public int[] ApprovedRequests { get; set; }
+        public DateTime CurrentMonth { get; set; }
+        public List<DateTime> Holidays { get; set; }
+        public List<DateTime> WorkingWeekends { get; set; }
+        public List<DateTime> PendingRequests { get; set; }
+        public List<DateTime> ApprovedRequests { get; set; }
+
+        public CalendarModel()
+        {
+            this.CurrentMonth = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            this.Holidays = null;
+            this.WorkingWeekends = null;
+            this.PendingRequests = null;
+            this.ApprovedRequests = null;
+        }
     }
 }

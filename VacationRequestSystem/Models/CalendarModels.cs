371a8f46adf9ea10794c7105f637ab56787393f0
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-08-03 18:21:29 +0300

    added checkbox for default Vacation type

--- VacationRequestSystem/Models/CalendarModels.cs
+++ VacationRequestSystem/Models/CalendarModels.cs
@@ -13,6 +13,7 @@ namespace VacationRequestSystem.Models
         public List<DateTime> WorkingWeekends { get; set; }
         public List<DateTime> PendingRequests { get; set; }
         public List<DateTime> ApprovedRequests { get; set; }
+        public List<VacationType> VacationType { get; set; }        
 
         public CalendarModel(DateTime currentMonth, List<DateTime> holidays, List<DateTime> workingWeekends, List<DateTime> pendingRequests, List<DateTime> approvedRequests)
         {
@@ -30,6 +31,33 @@ namespace VacationRequestSystem.Models
             this.WorkingWeekends = null;
             this.PendingRequests = null;
             this.ApprovedRequests = null;
+            this.VacationType = InitVacationTypes();
+        }
+
+        private List<VacationType> InitVacationTypes()
+        {
+            List<VacationType> listVacationType = new List<VacationType>();
+            listVacationType.Add(new VacationType(1, "Vacation", true));
+            listVacationType.Add(new VacationType(2, "Unpaid vacation", false));
+            listVacationType.Add(new VacationType(3, "Sick leave", false));
+            listVacationType.Add(new VacationType(4, "Personal", false));
+            listVacationType.Add(new VacationType(5, "Pregnancy", false));
+
+            return listVacationType;
+        }
+    }
+
+    public class VacationType
+    {
+        public int VacationTypeId { get; set; }
+        public string VacationTypeName { get; set; }
+        public bool IsSelected { get; set; }
+
+        public VacationType(int VacationTypeId, string VacationTypeName, bool IsSelected)
+        {
+            this.VacationTypeId = VacationTypeId;
+            this.VacationTypeName = VacationTypeName;
+            this.IsSelected = IsSelected;
         }
     }
 }
